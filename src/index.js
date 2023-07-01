bfaa7cf261df4741166ddd5f612f3f6e04670892
thanh <vtp5334@gmail.com>
2023-07-01 12:22:18 +0200

    commit moi

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 const port = 3000
+
+app.use(morgan('combined'))
 
 app.get('/', (req, res) =>{
     
-    res.send('moi hoc lap trinh a')
+    res.send('moi hoc lap trinh a co gang len')
 })
 
 app.listen(port, () => {
